641863e79436069c80b1eccba648a14cdd330c93
HariShankarSrungavarapu <s.harishankar333@gmail.com>
2016-08-30 13:51:53 +0530

    Added Validations to Post Forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,24 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show]
+
   def new
-  	
+    @post = Post.new  	
   end
 
   def create
     @post = Post.new(post_params)
-    @post.save
-    redirect_to @post 
+    if @post.save
+      redirect_to @post 
+    else
+      render 'new'
+    end
   end
 
   def show
   end
 
   def index
-  	@posts = Post.all.order("created_at ASC")
+  	@posts = Post.all.order("created_at DESC")
   end
 
   private
